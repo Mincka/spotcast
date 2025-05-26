c2e27dfff358b541096bc14843238454662baf68
Felix Cusson <felixcusson@gmail.com>
2025-05-26 19:11:47 -0400

    Bump version: 6.0.0-a9 → 6.0.0-a10

--- custom_components/spotcast/__init__.py
+++ custom_components/spotcast/__init__.py
@@ -24,7 +24,7 @@ from .websocket import async_setup_websocket
 from .config_flow import DEFAULT_OPTIONS
 from .spotify import SpotifyAccount
 
-__version__ = "6.0.0-a9"
+__version__ = "6.0.0-a10"
 
 
 LOGGER = getLogger(__name__)
